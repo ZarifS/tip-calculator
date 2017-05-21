2aa305a227889d082e9830d5fe3aa4521eb7bf84
Zarif Shahriar <Zarif@Zarifs-MacBook-Pro.local>
2017-05-21 15:26:43 -0400

    Suggestion fragment complete.

--- app/src/main/java/com/zarifshahriar/tipcalculator/Suggestion.java
+++ app/src/main/java/com/zarifshahriar/tipcalculator/Suggestion.java
@@ -2,11 +2,17 @@ package com.zarifshahriar.tipcalculator;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Suggestion extends Fragment {
+
+    private RatingBar rating;
+    private TextView tip;
 
     public static Suggestion newInstance() {
         Suggestion fragment = new Suggestion();
@@ -21,7 +27,21 @@ public class Suggestion extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.suggestion_frag, container, false);
+        View rootView = inflater.inflate(R.layout.suggestion_frag, container, false);
+        rating = (RatingBar) rootView.findViewById(R.id.ratingBar);
+        tip = (TextView) rootView.findViewById(R.id.tip);
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            String text;
+            Double tipPercent = 10.0;
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                tipPercent = 10.0 + (v*2);
+                Log.i("Rating changed", "" + v + "" + "Should tip" + tipPercent);
+                text = Double.toString(tipPercent);
+                tip.setText(text+"%");
+            }
+        });
+        return rootView;
     }
 
 }
--- app/src/main/java/com/zarifshahriar/tipcalculator/Summary.java
+++ app/src/main/java/com/zarifshahriar/tipcalculator/Summary.java
@@ -9,12 +9,12 @@ import android.widget.TextView;
 
 public class Summary extends Fragment {
 
-    private View view;
     private TextView billAmount;
     private TextView finalBill;
     private TextView totalTip;
     private TextView tipPerPerson;
     private TextView peoplePay;
+    private View personLayout;
 
     public static Summary newInstance() {
         Summary fragment = new Summary();
@@ -24,28 +24,33 @@ public class Summary extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String bill = getArguments().getString("billAmount");
-        String tip = getArguments().getString("tipAmount");
-        String people = getArguments().getString("peopleAmount");
-        Double billAmount = Double.parseDouble(bill);
-        Double tipAmount = Double.parseDouble(tip);
-        int peopleAmount = Integer.parseInt(people);
-        calcAmounts(billAmount,tipAmount,peopleAmount);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.summary_frag, container, false);
+        View rootView = inflater.inflate(R.layout.summary_frag, container, false);
+        billAmount = (TextView) rootView.findViewById(R.id.bill);
+        finalBill = (TextView) rootView.findViewById(R.id.total_bill);
+        totalTip = (TextView) rootView.findViewById(R.id.tip);
+        personLayout = rootView.findViewById(R.id.person_summary);
+        peoplePay = (TextView) rootView.findViewById(R.id.bill_per_person);
+        tipPerPerson = (TextView) rootView.findViewById(R.id.tip_per_person);
+
+        String bill = getArguments().getString("billAmount");
+        String tip = getArguments().getString("tipAmount");
+        String people = getArguments().getString("peopleAmount");
+        System.out.println(tip+bill+people);
+        Double billAmount = Double.parseDouble(bill);
+        Double tipAmount = Double.parseDouble(tip);
+        int peopleAmount = Integer.parseInt(people);
+        calcAmounts(billAmount,tipAmount,peopleAmount);
+        return rootView;
     }
 
     private void calcAmounts(Double bill, Double tip, int people) {
         Double tipAmount = bill * tip/100;
         Double finalAmount = tipAmount + bill;
-
-        billAmount = (TextView) getView().findViewById(R.id.bill);
-        finalBill = (TextView) getView().findViewById(R.id.total_bill);
-        totalTip = (TextView) getView().findViewById(R.id.tip);
 
         System.out.println((Double.toString(bill)));
         System.out.println((Double.toString(tipAmount)));
@@ -55,12 +60,9 @@ public class Summary extends Fragment {
         totalTip.setText(Double.toString(tipAmount));
         finalBill.setText(Double.toString(finalAmount));
 
-        View personLayout = getView().findViewById(R.id.person_summary);
         if(people > 1) {
             Double perPerson = finalAmount/people;
             Double tipDivided = tipAmount/people;
-            peoplePay = (TextView) getView().findViewById(R.id.bill_per_person);
-            tipPerPerson = (TextView) getView().findViewById(R.id.tip_per_person);
             peoplePay.setText(Double.toString(perPerson));
             tipPerPerson.setText(Double.toString(tipDivided));
             personLayout.setVisibility(View.VISIBLE);
